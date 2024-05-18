44c88404a3e857ec710a496a327e06344ab614e7
Alberto <btocarmona@gmail.com>
2024-05-18 20:27:16 -0300

    Animacion-contacto

--- contacto.js
+++ contacto.js
@@ -1,24 +1,18 @@
-function validarContacto(){
+function validarContacto() {
 
     //validacion del email
-    mail = document.getElementById('email').value
-    objetoMail = document.getElementById('email')
-    console.log(objetoMail)
+    let mail = document.getElementById('email').value
 
-    if(!(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(mail))){
+    if (!(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(mail))) {
         alert("es invalido el email, reescribalo")
-    }else{
-        alert("el mail es valido")
     }
 
     //validacion telefono
-    telefono = document.getElementById('telefono').value.replace(/ /g, "")
-   
-    if(!(/^(\d{10}|\d{11})$/.test(telefono))){
-        alert("el telefono es invalido, por favor ingrese un nuemro de 10 u 11 digitos")
-    }else{
-        alert('el telefono funca')
+
+    let telefono = document.getElementById('telefono').value.replace(/ /g, "")
+
+    if (!(/^(\d{10}|\d{11})$/.test(telefono))) {
+        alert("el teléfono es invalido, por favor ingrese un numero de 10 u 11 digitos")
     }
 
-    alert("el fin")
 }
