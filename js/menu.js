a80694c1b9930a04a66c43f1e5fea3a1dce044b9
Alberto <btocarmona@gmail.com>
2024-05-25 18:20:49 -0300

    Bloqueo Scroll

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,31 @@
 const botonAbrirMenu = document.querySelector('.menu_icon');
 const menu = document.querySelector('#menu_nav');
-const menu_cerrar = document.querySelector('.cerrar');
+const oferta = document.querySelector('.oferta');
 
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     setTimeout(() => {
+//         oferta.classList.add('active_oferta')
+//     }, 1000);
+//
+//     setTimeout(() => {
+//         oferta.classList.remove('active_oferta')
+//     }, 8000);
+// })
 
 botonAbrirMenu.addEventListener('click', (ev) => {
     menu.classList.toggle('active');
 
+    if (menu.classList.contains('active')){
+        window.addEventListener('touchmove', cancelarEvento, { passive: false });
+    }else{
+        window.removeEventListener('touchmove', cancelarEvento);
+    }
 })
 
-menu_cerrar.addEventListener('click', () => {
-    menu.classList.toggle('active');
 
-})
+function cancelarEvento(ev){
+    ev.preventDefault();
+}
+
+
