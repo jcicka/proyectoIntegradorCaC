cb7361fd05317480bd59913696b9f2263face22a
Alberto <btocarmona@gmail.com>
2024-05-25 18:28:51 -0300

    Bloqueo Scroll + comentarios

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
 const botonAbrirMenu = document.querySelector('.menu_icon');
 const menu = document.querySelector('#menu_nav');
-const menu_cerrar = document.querySelector('.cerrar');
+const oferta = document.querySelector('.oferta');
 
-// CONTROLA Y HACE EL CAMBIO EN  EL MENU BAR EN EL MODO CELULAR
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     setTimeout(() => {
+//         oferta.classList.add('active_oferta')
+//     }, 1000);
+//
+//     setTimeout(() => {
+//         oferta.classList.remove('active_oferta')
+//     }, 8000);
+// })
+
 botonAbrirMenu.addEventListener('click', (ev) => {
     menu.classList.toggle('active');
 
+    if (menu.classList.contains('active')){
+        window.addEventListener('touchmove', cancelarEvento, { passive: false });
+    }else{
+        window.removeEventListener('touchmove', cancelarEvento);
+    }
 })
 
-menu_cerrar.addEventListener('click', () => {
-    menu.classList.toggle('active');
 
-})
+function cancelarEvento(ev){
+    ev.preventDefault();
+}
